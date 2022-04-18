2fbfe0ea5e80e6e5175a2cf09b90f34aca4c8ad5
DDoS_LING <stgzs.zy@qq.com>
2022-04-18 16:54:00 +0800

    日常更新，优化体验

--- js/download.js
+++ js/download.js
@@ -1,11 +1,41 @@
-function GetQueryString(name){
-    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+function GetQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
     var r = window.location.search.substr(1).match(reg);
-    if(r!=null)return decodeURI(decodeURI(r[2])); return null;
+    if (r != null) return decodeURI(decodeURI(r[2])); return null;
 };
 
-var dw_service=GetQueryString("dw");
+var dw_service = GetQueryString("dw");
 
-$("#vanilla").load("./"+dw_service+"/vanilla.html");
-$("#craftbukkit").load("./"+dw_service+"/craftbukkit.html");
-$("#spigot").load("./"+dw_service+"/spigot.html");
+if (dw_service == null || dw_service == "") {
+    dw_service = "lanzou";
+} else if (dw_service != "lanzou" && dw_service != "123pan" && dw_service != "aliyun") {
+    mdui.dialog({
+        title: 'Error',
+        content: '找不到对应的下载服务器',
+        buttons: [
+            {
+                text: '确认',
+            }
+        ],
+        onClosed: function (inst) {
+            dw_service = "lanzou";
+            display();
+            mdui.snackbar({
+                message: '已使用默认的蓝奏云下载源',
+                position: 'right-top',
+                buttonText: '更换',
+                onButtonClick: function () {
+                    location.href = "/?dw=noset";
+                }
+            });
+        }
+    });
+}
+display();
+
+function display() {
+    $("#vanilla").load("./" + dw_service + "/vanilla.html");
+    $("#craftbukkit").load("./" + dw_service + "/craftbukkit.html");
+    $("#spigot").load("./" + dw_service + "/spigot.html");
+    $("#dw-name").load("./" + dw_service + "/name.html");
+}
